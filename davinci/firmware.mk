ee6a1fe87c7d094e564665f8fdd090bf09940da7
lamjiidii1 <lamjiidii1@gmail.com>
2021-03-01 13:05:43 +0000

    upload it

---
+++ davinci/firmware.mk
@@ -0,0 +1,21 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/firmware/davinci/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi/firmware/davinci/aop.mbn:install/firmware-update/aop.mbn \
+    vendor/xiaomi/firmware/davinci/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi/firmware/davinci/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi/firmware/davinci/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi/firmware/davinci/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi/firmware/davinci/dspso.bin:install/firmware-update/dspso.bin \
+    vendor/xiaomi/firmware/davinci/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi/firmware/davinci/imagefv.elf:install/firmware-update/imagefv.elf \
+    vendor/xiaomi/firmware/davinci/km4.mbn:install/firmware-update/km4.mbn \
+    vendor/xiaomi/firmware/davinci/logo.img:install/firmware-update/logo.img \
+    vendor/xiaomi/firmware/davinci/multi_image.mbn:install/firmware-update/multi_image.mbn \
+    vendor/xiaomi/firmware/davinci/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi/firmware/davinci/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+    vendor/xiaomi/firmware/davinci/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi/firmware/davinci/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/xiaomi/firmware/davinci/uefi_sec.mbn:install/firmware-update/uefi_sec.mbn \
+    vendor/xiaomi/firmware/davinci/xbl.elf:install/firmware-update/xbl.elf \
+    vendor/xiaomi/firmware/davinci/xbl_config.elf:install/firmware-update/xbl_config.elf
